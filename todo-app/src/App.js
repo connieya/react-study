94d372ba33dc61b94ed98ee2a68990eb072ad712
gunny6026 <gunny6026@naver.com>
2022-07-12 00:16:58 +0900

    feat : toggle 버튼 (props  이해)

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,27 +4,11 @@ import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
 
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "리엑트의 기초 알아보기",
-      checked: true,
-    },
-    {
-      id: 2,
-      text: "컴포넌트 스타일링 해보기",
-      checked: true,
-    },
-    {
-      id: 3,
-      text: "일정 관리 앱 만들어 보기",
-      checked: false,
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   // 고유값으로 사용될 id
   // ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(1);
 
   const onInsert = useCallback(
     (text) => {
@@ -39,10 +23,28 @@ const App = () => {
     [todos]
   );
 
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos]
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo
+        )
+      );
+    },
+    [todos]
+  );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} toggleBtn={onToggle} />
     </TodoTemplate>
   );
 };
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,11 +1,16 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove, toggleBtn }) => {
   return (
     <div className='TodoList'>
       {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} />
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          removeList={onRemove}
+          onToggle={toggleBtn}
+        />
       ))}
     </div>
   );
