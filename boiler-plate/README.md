1ed6179f6ec57cec3c4fd966711f06c6cdc33101
gunny6026 <gunny6026@naver.com>
2022-07-15 10:13:35 +0900

    chore : nodemon 설치 , 의존성 추가

--- boiler-plate/README.md
+++ boiler-plate/README.md
@@ -2,4 +2,22 @@
 
 ## npm
 
-npm install mongoose --save
+- npm install mongoose --save
+- npm install body-parser --save
+- npm install nodemon --save-dev (dev 를 붙이면 develope 모드 )
+- npm run start
+
+## git
+
+-git rm --cached node_modules -r 
+
+(이미 stage aread 에 올라간 파일을 지우는 것)
+
+## nodemon
+
+소스를 변경할 때 그걸 감지해서
+자동으로 서버를 재 시작해주는 tool
+
+시작할 때 nodemon 으로 시작하려면 
+
+package.json 의 script 를 수정해야 한다.
--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,22 +1,42 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded 와 같이 생긴 것들을 분석해서 가져오게 함
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application.json 을 분석하게 해준다.
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://pgh:1234@cluster0.ww4o2.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParse: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndmodify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("mongo db connected"))
   .catch((err) => console.log("err = ", err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
