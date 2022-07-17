8d262174de252a885f6b8fec122fa711510c4a7a
gunny6026 <gunny6026@naver.com>
2022-07-17 14:35:33 +0900

    feat : create react-app

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,7 +1,49 @@
+import { useState } from "react";
 import "./App.css";
+import Counter from "./components/Counter";
+import Todos from "./components/Todos";
+import Modal from "react-modal";
+import CounterContainer from "./containers/CounterContainer";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 function App() {
-  return <></>;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return (
+    <>
+      <CounterContainer number={0} />
+      <hr />
+      <Todos />
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel='Example Modal'
+      >
+        <button onClick={closeModal}>close</button>
+        <button>편집실</button>
+        <button>나가기</button>
+      </Modal>
+    </>
+  );
 }
 
 export default App;
