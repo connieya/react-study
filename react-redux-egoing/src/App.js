f4450197da3db82aaa7453ec9852f4aa234d0e48
gunny6026 <gunny6026@naver.com>
2022-07-19 21:06:05 +0900

    feat : redux 없이 컴포넌트 간 의존성 연결 코드
    생활 코딩 리덕스 강의 예제 코드를 따라 치고 학습 하는 중
    redux 없이 컴포넌트 구성

---
+++ react-redux-egoing/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React, { Component } from "react";
+import AddNumberRoot from "./components/AddNumberRoot";
+import DisplayNumberRoot from "./components/DisplayNumberRoot";
+
+function App() {
+  return (
+    <div>
+      <h1>Root</h1>
+      <AddNumberRoot />
+      <DisplayNumberRoot />
+    </div>
+  );
+}
+
+export default App;
