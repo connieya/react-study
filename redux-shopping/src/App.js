5b7b846bc5e5cae4b194ab29abc0cec898e4cc7c
gunny6026 <gunny6026@naver.com>
2022-09-03 01:28:47 +0900

    feat :  상품 목록

--- redux-shopping/src/App.js
+++ redux-shopping/src/App.js
@@ -1,7 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./containers/Header";
 import "./App.css";
+import ProductDetails from "./containers/ProductDetail";
+import ProductListing from "./containers/ProductListing";
 
 function App() {
-  return <div className='App'>dd</div>;
+  return (
+    <div>
+      <h1>dd</h1>
+      <Header />
+      <Routes>
+        <Route path='/' exact element={<ProductListing />}></Route>
+        <Route path='/product/:productId' element={<ProductDetails />}></Route>
+        <Route>404 Not Found!</Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
